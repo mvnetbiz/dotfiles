b9f778667d72f74bd6ddf64a996f4a37059064d7
Matt Votava <mvnetbiz@gmail.com>
2017-03-01 02:25:50 -0800

    Automatically get primary display for tray

--- .i3/makei3conf.sh
+++ .i3/makei3conf.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-cat ~/.i3/configs/config.{gaps,display,colors,appearance,binds,autostart} > ~/.i3/config
+xrandr -q | awk '/primary/ {printf "%s", "set $primary_display " ; print $1}' > ~/.i3/config
+cat ~/.i3/configs/config.{gaps,display,colors,appearance,binds,autostart} >> ~/.i3/config
