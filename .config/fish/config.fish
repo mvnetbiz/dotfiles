8c9a01a710593c3b3231b6ea843f4c3b28b46710
Matt Votava <mvnetbiz@gmail.com>
2017-03-01 02:30:49 -0800

    start powerline daemon in fish conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,10 @@
+set fish_greeting
+set -gx EDITOR vim
+
 set fish_function_path $fish_function_path "/usr/lib/python3.6/site-packages/powerline/bindings/fish"
 powerline-setup
+
+if not pgrep -fx "/usr/bin/python /usr/bin/powerline-daemon -q" 1>/dev/null
+        powerline-daemon -q
+end
+
