a0d9c5309dcf19bc3b8f9de7c73420a3acef9922
Matt Votava <mvnetbiz@gmail.com>
2017-02-28 17:22:52 -0800

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,2 @@
+set fish_function_path $fish_function_path "/usr/lib/python3.6/site-packages/powerline/bindings/fish"
+powerline-setup
---
+++ .i3/makei3conf.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cat ~/.i3/configs/config.{gaps,display,colors,appearance,binds,autostart} > ~/.i3/config
