47f70de68fd311e4cbc8d21b21e6f7ec161545ab
Matt Votava <mvnetbiz@gmail.com>
2017-03-18 16:58:14 -0700

    tweak fade time

--- bin/.bin/fadekill.bash
+++ bin/.bin/fadekill.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i in {1..100}
+for i in {1..50}
 do
-	transset-df --dec .01 -i $1
-	sleep .005
+	transset-df --dec .02 -i $1
+	sleep .008
 done
 xkill -id $1
--- bin/.bin/toggledark.bash
+++ bin/.bin/toggledark.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 bash ~/.bin/fehscrn.bash &
-sleep .2
-if [ "$(gsettings get org.gnome.desktop.interface gtk-theme)" = "'Arc'" ]; then gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark" ; else gsettings set org.gnome.desktop.interface gtk-theme "Arc"; fi
+sleep .3
+if [ "$(gsettings get org.gnome.desktop.interface gtk-theme)" = "'Arc'" ]
+then
+gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark"
+else
+gsettings set org.gnome.desktop.interface gtk-theme "Arc"
+fi
 
