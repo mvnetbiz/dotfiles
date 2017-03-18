b4e94647b116c84ccd12a05d5db8dd174de09e50
Matt Votava <mvnetbiz@gmail.com>
2017-03-17 22:46:03 -0700

    add ls abbreviation to fish

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+set fish_greeting
+set -gx EDITOR vim
+
+set fish_function_path $fish_function_path "/usr/lib/python3.6/site-packages/powerline/bindings/fish"
+powerline-setup
+
+if not pgrep -fx "/usr/bin/python /usr/bin/powerline-daemon -q" 1>/dev/null
+        powerline-daemon -q
+end
+
+abbr l "ls -lhgG --time-style='+' --color=auto"                                                      
+
+
