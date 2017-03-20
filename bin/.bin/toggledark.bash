994dd326ca55a71ca6293220557378c5eb31d131
Matt Votava <mvnetbiz@gmail.com>
2017-03-20 14:16:59 -0700

    remove fade on the toggle theme script

--- bin/.bin/toggledark.bash
+++ bin/.bin/toggledark.bash
@@ -1,6 +1,4 @@
 #!/bin/bash
-bash ~/.bin/fehscrn.bash &
-sleep .2
 if [ "$(gsettings get org.gnome.desktop.interface gtk-theme)" = "'Arc'" ]
 then
 gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark"
